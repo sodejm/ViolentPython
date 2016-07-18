bedd707885d1515f55ab9c6cbcf00edb8984b052
justin soderberg <sodejm@gmail.com>
2016-07-18 12:15:56 -0700

    reformat

--- anon-ftp.py
+++ anon-ftp.py
@@ -1,20 +1,22 @@
 import ftplib
 import optparse
 
+
 def anonLogin(hostname):
     try:
         ftp = ftplib.FTP(hostname)
         ftp.login('anonymous', 'me@example.com')
-        print ('\n[*]'+str(hostname)+' FTP Anonymous Logon Successful')
+        print ('\n[*]' + str(hostname) + ' FTP Anonymous Logon Successful')
         ftp.quit()
         return True
     except Exception, e:
-        print('\n[-]' + str(hostname)+' FTP Anonymous Logon Failed')
+        print('\n[-]' + str(hostname) + ' FTP Anonymous Logon Failed')
         return False
-    # host = '192.168.95.179'
-    # anonLogin(host)
+        # host = '192.168.95.179'
+        # anonLogin(host)
 
 
+# added in a file reader
 def main():
     parser = optparse.OptionParser('uasge%prog -F <host list file>')
     parser.add_option('-F', dest='hostFile', type='string', help='Name of the host file')
